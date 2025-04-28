0a959edda183f8be9725d32d5adce83d9ee454a4
zonzin <rodrigojzonzin@gmail.com>
2025-04-28 14:54:23 -0300

    commit 28-04-2025 by rodrigo

--- ex2/analise_ag.py
+++ ex2/analise_ag.py
@@ -2,9 +2,7 @@ import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-import sys, os
-
+import pandas as pd
 
 class Gene:
     def __init__(self, alelos):
@@ -17,24 +15,19 @@ class Gene:
     def shape(self):
         return self.alelos_dim
 
-
 class AG:
-    def __init__(self, pop_size=10, gene_size=40, taxa_mutacao=0.01, intervalo=(-2, 2), minMaxIndiv = (-20, 20)):
+    def __init__(self, pop_size=10, gene_size=40, taxa_mutacao=0.01, taxa_cruzamento=1.0, intervalo=(-2, 2), minMaxIndiv=(-20, 20)):
         self.pop_size = pop_size
         self.gene_size = gene_size
         self.intervalo = intervalo
         self.minMaxIndiv = minMaxIndiv
         self.pop = self._gerar_populacao()
         self.taxa_mutacao = taxa_mutacao
-        
+        self.taxa_cruzamento = taxa_cruzamento  # Novo parâmetro para controle de cruzamento
 
     def _gerar_populacao(self):
         vmin, vmax = self.minMaxIndiv
         return [Gene([random.uniform(vmin, vmax) for _ in range(self.gene_size)]) for _ in range(self.pop_size)]
-
-    def print_pop(self):
-        for gene in self.pop:
-            print(gene)
 
     def fitness(self, gene):
         x = gene.alelos
@@ -52,10 +45,8 @@ class AG:
 
     def crossoverBLXAlphaBeta(self, paiX, paiY, alpha=0.75, beta=0.25):
         filhos = []
-
         d = [abs(paiX.alelos[i] - paiY.alelos[i]) for i in range(self.gene_size)]
-
-        for _ in range(2):  
+        for _ in range(2):
             alelos_filho = []
             for i in range(self.gene_size):
                 if self.fitness(paiX) <= self.fitness(paiY):
@@ -66,7 +57,6 @@ class AG:
                     x2 = min(paiX.alelos[i] + alpha * d[i], self.intervalo[1])
                 alelos_filho.append(random.uniform(x1, x2))
             filhos.append(Gene(alelos_filho))
-
         return filhos[0], filhos[1]
 
     def mutacao(self, gene):
@@ -76,92 +66,58 @@ class AG:
                 gene.alelos[i] = random.uniform(pmin, pmax)
         return gene
 
-    def mutacao_suave(self, gene):
-        for i in range(len(gene.alelos)):
-            if random.random() < self.taxa_mutacao:
-                perturbacao = random.gauss(0, 0.1)  
-                gene.alelos[i] += perturbacao
-
-                gene.alelos[i] = max(self.intervalo[0], min(self.intervalo[1], gene.alelos[i]))
-        return gene
-
-
-    def nova_geracao(self):
-        nova_pop = []
-        while len(nova_pop) < self.pop_size:
-            pai1 = self.roleta()
-            pai2 = self.roleta()
-            while pai1 == pai2:
-                pai2 = self.roleta()
-
-            filho1, filho2 = self.crossoverBLXAlphaBeta(pai1, pai2)
-
-            nova_pop.append(self.mutacao_suave(filho1))
-
-            if len(nova_pop) < self.pop_size:
-                nova_pop.append(self.mutacao_suave(filho2))
-
-        self.pop = nova_pop
-
     def nova_geracao_elitismo(self):
         nova_pop = [self.melhor_individuo()]
-
         while len(nova_pop) < self.pop_size:
             pai1 = self.roleta()
             pai2 = self.roleta()
-
-            filho1, filho2 = self.crossoverBLXAlphaBeta(pai1, pai2)
+            if random.random() < self.taxa_cruzamento:
+                filho1, filho2 = self.crossoverBLXAlphaBeta(pai1, pai2)
+            else:
+                filho1, filho2 = pai1, pai2
 
             nova_pop.append(self.mutacao(filho1))
-
             if len(nova_pop) < self.pop_size:
                 nova_pop.append(self.mutacao(filho2))
-
         self.pop = nova_pop
 
     def melhor_individuo(self):
         return min(self.pop, key=self.fitness)
 
+# -------------------------------------
+# Parte para rodar o experimento fatorial
+# -------------------------------------
 
-if __name__ == "__main__":
-    #random.seed(42)
-    os.makedirs('resultsMutacao', exist_ok=True)
-    
-    n_execucoes = [0.001, 0.01, 0.05, 0.1, 0.15, 0.2]
-    results_execucoes = []
-    for exec in n_execucoes:
-        with open(f'resultsMutacao/logAG_{exec}.txt', 'w') as f:
-            sys.stdout = f
-            fitness_result = []
-            
-            ag = AG(pop_size=20, gene_size=5, taxa_mutacao=exec, intervalo=(-2, 2))
-            
-            geracoes = 60
-            for i in range(geracoes):
-                print(f"Geracao {i}")
-                ag.print_pop()
+# Configurações dos testes
+taxas_mutacao = [0.01, 0.05, 0.10]
+taxas_cruzamento = [0.6, 0.8, 1.0]
+populacoes = [25, 50, 100]
+geracoes_lista = [25, 50, 100]
 
-                melhor = ag.melhor_individuo()
-                print(f"\nMelhor individuo: {melhor}")
-                print(f"Fitness: {ag.fitness(melhor)}\n")
-                
-                fitness_result.append(ag.fitness(melhor))
-                ag.nova_geracao_elitismo()
+# Resultados
+resultados = []
 
-            results_execucoes.append(fitness_result)
+# Experimento
+for mutacao in taxas_mutacao:
+    for cruzamento in taxas_cruzamento:
+        for pop in populacoes:
+            for geracoes in geracoes_lista:
+                ag = AG(pop_size=pop, gene_size=3, taxa_mutacao=mutacao, taxa_cruzamento=cruzamento, intervalo=(-2, 2))
+                for _ in range(geracoes):
+                    ag.nova_geracao_elitismo()
+                melhor_fitness = ag.fitness(ag.melhor_individuo())
+                resultados.append({
+                    'Melhor Fitness': melhor_fitness,
+                    'Mutação': mutacao,
+                    'Cruzamento': cruzamento,
+                    'População': pop,
+                    'Gerações': geracoes
+                })
+                print(f"Feito: Mutação={mutacao}, Cruzamento={cruzamento}, População={pop}, Gerações={geracoes}, Fitness={melhor_fitness}")
 
+# Criar DataFrame
+df_resultados = pd.DataFrame(resultados)
+print(df_resultados)
 
-            sys.stdout = sys.__stdout__
-        
-
-    
-    plt.figure(figsize=(15, 10))
-    for i,exec in enumerate(n_execucoes):
-        plt.plot(range(geracoes), results_execucoes[i], label = f'Taxa Mutacao. {exec}')
-
-    plt.legend()
-    plt.xlabel("Gerações")
-    plt.ylabel("Fitness")
-    plt.savefig('resultsMutacao/fitness_mutacao.png')
-        
-    #print(results_execucoes)
+# Opcional: salvar em CSV
+df_resultados.to_csv('resultados_ag.csv', index=False)
